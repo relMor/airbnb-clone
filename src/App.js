84f12a6ce39f85cccf5ba138415ef69c7a0884a0
relMor <amanvarma.600847@gmail.com>
2020-08-27 19:26:31 +0530

    completed footer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="app">
      <Header />
      <Home />
+     <Footer />
     </div>
   );
 }
